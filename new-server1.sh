33e2eb2d62be8e7fd95bdcc7a163f73220687dfa
hummer74 <76160763+hummer74@users.noreply.github.com>
2024-02-14 00:19:47 +0500

    Add files via upload

--- new-server1.sh
+++ new-server1.sh
@@ -100,15 +100,73 @@ echo "Fix special files permissions"
 chmod 644 ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/known_hosts
 chmod 644 ~/.ssh/config
+rm ~/setup.7z
 echo ""
 echo ""
 echo ""
+echo "Install VLESS, Xray-Reality."
+read -p "Do you want to proceed? (y/n) " yn1
+if [[ "$yn1" = [y] && "$yn" -gt 0 ]]; then
+   echo "# Ok. Install VLESS, Xray-Reality."
+   read  -p  "Press Enter for process..."
+   bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install &&
 
-# rm ~/setup.7z
+else
+    echo "Ok. Go to next point..."
+    break
+fi
+echo ""
+echo ""
+echo ""
+echo "# Install 3X-UI. opossum, standard, port 33900."
+read -p "Do you want to proceed? (y/n) " yn2
+if [[ "$yn2" = [y] && "$yn" -gt 0 ]]; then
+   echo "# Ok. Install 3X-UI."
+   read  -p  "Press Enter for process..."
+   bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh) &&
 
-
+else
+    echo "Ok. Go to next point..."
+    break
+fi
+echo ""
+echo ""
+echo ""
+echo "# Install WireGuard, port 33901."
+read -p "Do you want to proceed? (y/n) " yn3
+if [[ "$yn3" = [y] && "$yn" -gt 0 ]]; then
+   echo "# Ok. Install 3X-UI."
+   read  -p  "Press Enter for process..."
+   curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+   chmod +x wireguard-install.sh
+   ./wireguard-install.sh &&
+   sysctl --system
+   systemctl restart wg-quick@wg0
+   systemctl status wg-quick@wg0
+else
+    echo "Ok. Go to next point..."
+    break
+fi
+echo ""
+echo ""
+echo ""
+echo "# Install OpenVPN, port 33902."
+read -p "Do you want to proceed? (y/n) " yn3
+if [[ "$yn3" = [y] && "$yn" -gt 0 ]]; then
+   echo "# Ok. Install 3X-UI."
+   read  -p  "Press Enter for process..."
+   curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
+   chmod +x openvpn-install.sh
+   ./openvpn-install.sh &&
+else
+    echo "Ok. Go to next point..."
+    break
+fi
+echo ""
+echo ""
+echo ""
 echo -e "\033[31mLast update and reboot...\033[0m"
-read  -p  "Press Enter for update and reboot..."
+read  -p  "Press Enter for last update and reboot..."
 apt update -y && apt full-upgrade -y && apt autoremove -y && apt autoclean && #reboot now
 echo ""
 echo ""
