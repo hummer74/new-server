1b1a3a035e494b9a7ccd1f78b30ded323a4e5e50
hummer74 <76160763+hummer74@users.noreply.github.com>
2026-02-17 08:30:43 +0500

    Add gpg to installation list in new-server.sh

--- new-server.sh
+++ new-server.sh
@@ -60,7 +60,7 @@ echo ""
 echo ""
 echo ""
 echo "# Install mc, curl, wget, htop, unattended-upgrades, apt-listchanges, fail2ban."
-apt install rsyslog mc curl wget unzip p7zip-full htop unattended-upgrades apt-listchanges bsd-mailx iptables fail2ban dos2unix locales screen dnsutils openssl -y &&
+apt install rsyslog mc curl wget unzip p7zip-full htop unattended-upgrades apt-listchanges bsd-mailx iptables fail2ban dos2unix locales screen dnsutils openssl gpg -y &&
 egrep "sudo mc" ~/.profile >/dev/null
 if [ $? -eq 0 ]; then
    echo "Midnight Commander exists!"
