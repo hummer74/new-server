08dd2ccd0d715e60a803d9d6206b5823d78474fc
hummer74 <hummer74rus@gmail.com>
2026-03-28 10:31:19 +0500

    MTProto added

---
+++ MTProto-Install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash -c "$(curl -L https://raw.githubusercontent.com/hummer74/new-server/main/telemt-install.sh)"
--- startinstall.sh
+++ startinstall.sh
@@ -1,3 +1,2 @@
-
-#!/bin/sh
+#!/bin/bash
 bash -c "$(curl -L https://raw.githubusercontent.com/hummer74/new-server/main/new-server.sh)"
