5702f36b986c9481c8f3adf180c6dd35e6acbe60
hummer74 <hummer74rus@gmail.com>
2026-03-28 12:00:18 +0500

    fix: Update fail2ban configuration for improved security

--- new-server.sh
+++ new-server.sh
@@ -146,37 +146,6 @@ echo ""
 echo ""
 sysctl --system
 
-# Configure Fail2ban (do not modify original jail.conf/fail2ban.conf)
-systemctl enable fail2ban.service
-
-# Create fail2ban.local to set allowipv6 (do not touch fail2ban.conf)
-cat > /etc/fail2ban/fail2ban.local <<EOF
-[Definition]
-allowipv6 = auto
-EOF
-
-# Create jail.local with main settings (do not touch jail.conf)
-cat > /etc/fail2ban/jail.local <<EOF
-[DEFAULT]
-bantime = 7d
-findtime = 120m
-maxretry = 3
-ignoreip = 176.56.1.165 95.78.162.177 45.86.86.195 46.29.239.23 45.38.143.206 176.125.243.194 194.58.68.23
-
-[sshd]
-enabled = true
-port = 22,24940
-EOF
-
-systemctl restart fail2ban.service
-systemctl status fail2ban.service
-printf "\033[33m# Don't forget to add the new SSH port (24940) in the client!\033[0m\n"
-grep --color 'Port ' /etc/ssh/sshd_config
-read -n1 -s -r -p "Press any key..."; echo
-echo ""
-echo ""
-echo ""
-
 # Download setup.7z and check success
 wget -O setup.7z https://raw.githubusercontent.com/hummer74/new-server/main/setup.7z
 if [ ! -f setup.7z ]; then
